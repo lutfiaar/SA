811ece2c9d642906448629169f509db5d05c0758
lutfiaar <lutfiaariyati@gmail.com>
2022-08-02 14:52:52 +0700

    migratin, model, install package

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -18,9 +19,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
         'email',
         'password',
+        'role_id'
+
     ];
 
     /**
@@ -33,6 +35,19 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+
+public $incrementing = false;
+
+protected $keyType = 'string';
+
+public static function boot(){
+    parent::boot();
+
+    static::creating(function ($issue) {
+        $issue->id = Str::uuid(36);
+    });
+}
+
     /**
      * The attributes that should be cast.
      *
@@ -41,4 +56,24 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getRoleNameAttribute()
+    {
+        return $this->roles->name;
+    }
+
+
+    public function customer()
+    {
+        return $this->hasOne(Customer::class,'user_id','id');
+    }
+    public function employee()
+    {
+        return $this->hasOne(Employee::class,'user_id','id');
+    }
+    public function roles()
+    {
+        return $this->hasOne(Roles::class,'id','role_id');
+    }
+
 }
