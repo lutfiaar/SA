6b1d91801953765ec02a5963625e5d42e730f4cf
tasya <tasyamaharani77@gmail.com>
2022-08-02 23:00:37 +0700

    config(auth,app)

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,16 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'api' => [
+            'driver' => 'token',
+            'provider' => 'users',
+            'hash' => false,
+        ],
+
+        'admin' => [
+            'driver' => 'session',
+            'provider' => 'admins',
+        ]
     ],
 
     /*
